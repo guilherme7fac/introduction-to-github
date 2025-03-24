19cc5f185df75464082874f6b2eb369330bfe796
guilherme7fac <guilhermeblack24@gmail.com>
2025-03-24 16:15:51 -0300

    Add files via upload

---
+++ pagina.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() 
+{
+    printf("1-iniciando o algotitmo para colocar o carro em movimento\n");
+        printf("2-Entrar no carro\n");
+            printf("3-ajustar o banco e os espelhos retrovisores\n");
+                printf("4-colocar o cinto de segurança\n");
+                    printf("5-Colocar o cambio em ponto\n ");
+                        printf("6-Ligar o carro\n");
+                    printf("7-Colocar o cambio na primeira marcha\n");
+                printf("8-Pisor no acelerador\n");
+            printf("9-soltar o frio de mão\n");
+        printf("10-soltar a embreagem devagar\n");
+    printf("11-finalizar o algaritmo\n");
+}
